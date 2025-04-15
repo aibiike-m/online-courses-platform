f447cd1e67923c65a0ad7ccadbc610989ad473d4
Bikoo0 <aibimadalieva06@gmail.com>
2025-04-15 22:46:31 +0600

    Profile, authorization and registration templates

--- courses/urls.py
+++ courses/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+from django.views.generic import RedirectView
 from courses import views
-app_name = 'courses'
+
+app_name = "courses"
 urlpatterns = [
-    path('search/', views.catalog, name='search'),
-    path('<slug:category_slug>/', views.catalog, name='index'),
-    path('course/<slug:course_slug>/', views.course, name='course'),
+    path("search/", views.catalog, name="search"),
+    path("<slug:category_slug>/", views.catalog, name="index"),
+    path("course/<slug:course_slug>/", views.course, name="course"),
+    # path("course/<slug:course_slug>/", views.course, name="course"),
 ]
